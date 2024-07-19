f06c0ea350f0237837914ff5e028bced1bd9e5d8
MenUrux <jorgepino.utu@gmail.com>
2024-07-19 17:40:13 +0000

    api

---
+++ src/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+some_data = { "name": "Bobby", "lastname": "Rixer" }
+
+""" @app.route('/todos', methods=['GET'])
+def hello_world():
+    # You can convert that variable into a json string like this
+    json_text = jsonify(some_data)
+
+    # And then you can return it to the front end in the response body like this
+    return json_text """
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    # You can convert that variable into a json string like this
+    json_text = jsonify(todos)
+
+    # And then you can return it to the front end in the response body like this
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print(request)
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    del todos[position]
+    return jsonify(todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
+
